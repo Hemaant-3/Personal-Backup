0ae2f5f99aa80609831af1ade13225645c09c20f
Hemaant-3 <hemantchuadhary4321@gmail.com>
2021-05-01 16:58:56 +0530

    update to my copde

--- ppslab2.c/function.dart
+++ ppslab2.c/function.dart
@@ -23,9 +23,16 @@ test_fun2(a, {s1: 22}) {
   print(s1);
 }
 
+/*
+lambda function
+*/
+printMsg() => print('Hello world');
+int test_n() => 123;
 void main() {
   // bb(12);
   // test_fun(123);
   // test_fun(myname: 'Hemant', sirname: 'Mionu');
-  test_fun2('45');
+  // test_fun2('45');
+  printMsg();
+  print(test_n());
 }

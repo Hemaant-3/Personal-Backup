3673b055d064ffd09814fe53dddcc00339ef8aa9
Hemaant-3 <hemantchuadhary4321@gmail.com>
2021-05-01 16:16:35 +0530

    my change comments

--- ppslab2.c/enum.dart
+++ ppslab2.c/enum.dart
@@ -4,6 +4,12 @@ enum object {
   man,
   women,
 }
+/*
+enumeration data types easily acccess the element defined in enum constructor 
+as we can access by index values and by their name 
+forEach function used to iterate multiple elements,
+at once.
+*/
 main(List<String> args) {
   print(object.values);
   object.values.forEach((v) => print('values: $v, index: ${v.index}'));

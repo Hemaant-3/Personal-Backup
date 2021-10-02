ef79fa2880654a572449de19b08e3957f621d68b
Hemaant-3 <hemantchuadhary4321@gmail.com>
2021-10-02 18:47:08 +0530

    hemat

--- Python/basic.py
+++ Python/basic.py
@@ -1,3 +1,34 @@
-print(4*(" Hemant\n\n\n\n\n\n"));
-a = "Hemantkumarchaudhary"
-print(a[1:9])
+from math import *
+def power (x, y):
+	return pow(x, y)
+	"""This is given power"""
+def add(x,y):
+        return x+y
+def multiply(x,y):
+    return x*y
+def substract(x,y):
+    return x-y
+def divide(x,y):
+    return x/y
+	#take input from the user
+print(".Select operation")
+print("1.add")
+print("2.substract")
+print("3.multiply")
+print("4.division")
+print("5power")
+choice = input("Enter the choice (1,2,3,4,5):\n\n")
+num1 =int(input ("Enter the first number: \n"))
+num2 = int(input("Enter the second number: \n"))
+if choice == '1':
+		print (num1, "+", num2,"=", add(num1,num2))
+elif choice == '2':
+		print (num1, "-", num2,"=", substract (num1,num2))
+elif choice == '3':
+		print (num1, "*", num2,"=", multiply (num1,num2))
+elif choice == '4':
+		print (num1, "÷", num2,"=", divide(num1,num2))
+elif choice == '5':
+		print (num1, "^", num2,"=", power(num1,num2))
+else:
+		print("invalid input")

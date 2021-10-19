d649017c0a5f74b5ce8b68401b8a6c1337a116e6
Hemaant-3 <hemantchuadhary4321@gmail.com>
2021-10-19 18:47:47 +0530

    klhjkg

--- Python/gme.py
+++ Python/gme.py
@@ -1,17 +1,9 @@
-# n = input("enter any number")
-# for i in range(n):
-import base64
-# n = ["A","B","c"]
-n = input("enter anything\n")
-a = [1]
-#to encode
-for i in range(len(n)):
-    # print(ord(n[i]))
-    a[i] = ord(n[i])
-    a.append(a[i])
-    print(a[i])
-
-# decode 
-for i in range(len(n)):
-    print(chr(a[i]),end='')
-    
+c = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+a = []
+for i in range(len(c)):
+    a = ord(c[i])
+    print(a, end=' ')
+    b = chr(ord(c[i]))
+    print(b,end=' ')
--- Python/rough.py
+++ Python/rough.py
@@ -1,11 +1,27 @@
 import base64
-def(a):
+# def(a):
 
-   a = input("enter any chr\n")
-   enc = []
-   print(ord(a))
-   print(chr(ord(a)))
-   enc_c = chr(ord(a))
-   enc.append(enc_c)
+#    a = input("enter any chr\n")
+#    enc = []
+#    print(ord(a))
+#    print(chr(ord(a)))
+#    enc_c = chr(ord(a))
+#    enc.append(enc_c)
 
-     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+#      return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def encode(key, clear):
+  enc = []
+  for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) +
+                     ord(key_c))%256)
+
+        enc.append(enc_c)
+        print(ord(enc[i]),end=" ")
+        # print(enc[i],end=" ")
+key = input("enter key\n")
+clear = input("message\n")
+encode(key, clear)
+
+  

3b4df308805fd930c679501e64fdb58d52d20c91
Hemaant-3 <Hemantchaudhary4321@gmail.com>
2021-11-28 15:36:50 +0530

    re

--- pointer/dellinked.c
+++ pointer/dellinked.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
-    int t, choice;
  typedef struct node
     {
         int data;
         struct node *next;
             }node;
-
+int main()
+{
+    int t, choice;
 node *newnode, *head, *temp;
+head = 0;
 
+while(choice){
 newnode = (node*)malloc(sizeof(node));
 printf("Enter the data: ");
 scanf("%d", &newnode->data);
@@ -21,28 +22,38 @@ if (head==0)
     head = temp = newnode;
 } else
 {
-    temp = newnode->next;
+    temp->next = newnode;
+ 
     temp = newnode;
 }
+printf("enter 0 for exit and 1 enter a newnode: ");
+scanf("%d", &choice);
+}
+
 temp = head;
 
-printf("list before deletion=> ");
+printf(" list before deletion\n=> ");
 
-if(temp!=0){
+while(temp!=0){
     printf("%d, ", temp->data);
     temp = temp->next;
 }
-printf("Enter 1 for delete first element from list and 0 for skip deletion: ");
+printf("\nEnter 1 for delete first element from list and 0 for skip deletion: ");
 scanf("%d", &t);
 /* deleting at begning */
 
-
 if(t){
-printf("list after deletion=> ");
-if(temp!=0){
+temp = head;
+head = head->next; //head++;
+free(temp);
+}
+
+printf("list after deletion\n=> ");
+temp = head;
+while(temp!=0){
     printf("%d, ", temp->data);
     temp = temp->next;
 }
-}
+
 return 0;
 }

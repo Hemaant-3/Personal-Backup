3916435e79d4de047c8d49f31fbd5d52caa6d63d
Hemaant-3 <Hemantchaudhary4321@gmail.com>
2021-12-14 12:57:02 +0530

    object and classes

---
+++ Python/15/12/21.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(abc,name,rollno,course):
+        abc.name = name
+        abc.rollno = rollno
+        abc.course = course
+        print(abc.name,abc.rollno,abc.course)
+
+s1 = Student("Hemant", 2005080100046, "B.tech")
+s2 = Student("Pranjal singh",2005080100065, "B.Tech")
+s2 = Student("Pranjal yadav",2005080100065, "B.Tech")
+s2 = Student("Harshit mishra",2005080100065, "B.Tech")
+s2 = Student("Rahul Singh",2005080100072, "B.Tech")
+s2 = Student("Prakhar Gupta",2005080100063, "B.Tech")
+s2 = Student("prakhar madhava srivastav",2005080100065, "B.Tech")
+s2 = Student("Mohit Narayan",2005080100065, "B.Tech")
+s2 = Student("Kunal yadav",2005080100065, "B.Tech")
+s2 = Student("sadiq Mansoori",2005080100065, "B.Tech")

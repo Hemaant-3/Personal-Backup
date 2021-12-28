1ae4b2cadfc62cae79e1c6e84f8e8ef4e1a237f4
Hemaant-3 <Hemantchaudhary4321@gmail.com>
2021-12-28 12:49:17 +0530

    df

---
+++ CP array/kthmaxmin.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int a[] = {1,2,3,4,5,6,7,8};
+int k;
+printf("enter the k element: ");
+scanf("%d", &k);
+for (int i = 0; i < 8; i++)
+{
+    for (int j = i+1; j < 8; j++)
+    {
+        if(a[i]>a[j])
+        {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
+
+    printf("the Min kth element is: %d", a[k-1]);
+    printf("\nthe MAx kth element is: %d", a[7]);
+
+
+return 0;
+}
